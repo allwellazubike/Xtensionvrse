2fb76a9dc7e39a67c3ac9f014413c1f8f99deff7
Allwell <allwellazubike@gmail.com>
2026-01-17 22:31:33 +0100

    ...

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,8 @@ import Cart from "./pages/Cart";
 
 function App() {
   const [darkMode, setDarkMode] = useState(false);
+
+  
   // function to toggle dark mode
   const toggleDarkMode = () => {
     setDarkMode(!darkMode);
