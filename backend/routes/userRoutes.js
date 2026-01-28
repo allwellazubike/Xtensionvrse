6fafa0dad89b1ac714ed5a993e1ca6df4a8b8c4b
Allwell <allwellazubike@gmail.com>
2026-01-28 21:30:33 +0100

    route auth

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -6,6 +6,7 @@ import passportLocal from "passport-local";
 import { Strategy } from "passport-local";
 import { verifyPassword, hashPassword } from "../controllers/hashController.js";
 
+const app = express(); 
 const router = express.Router();
 
 app.use(expressSession({
@@ -43,40 +44,45 @@ router.post("/create", async (req, res) => {
 
 // login user
 router.post("/login", async (req, res) => {
-  try {
-    const { email, password } = req.body;
-    const result = await db.query("SELECT * FROM users WHERE email = $1", [
-      email,
-    ]);
-    if (!result.rows[0])
-      return res.status(404).json({ error: "User not found" });
+//   try {
+//     const { email, password } = req.body;
+//     const result = await db.query("SELECT * FROM users WHERE email = $1", [
+//       email,
+//     ]);
+//     if (!result.rows[0])
+//       return res.status(404).json({ error: "User not found" });
 
-    const isValidPassword = await verifyPassword(
-      password,
-      result.rows[0].password_hash,
-    );
-    if (!isValidPassword) {
-      console.log("invalid credentials");
-      return res.status(401).json({ error: "Invalid credentials" });
-    }
+//     const isValidPassword = await verifyPassword(
+//       password,
+//       result.rows[0].password_hash,
+//     );
+//     if (!isValidPassword) {
+//       console.log("invalid credentials");
+//       return res.status(401).json({ error: "Invalid credentials" });
+//     }
 
-    console.log("login success");
-    res.json({
-      message: "Login successful",
-      user: { name: result.rows[0].full_name },
-    });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: "Login failed" });
-  }
+//     console.log("login success");
+//     res.json({
+//       message: "Login successful",
+//       user: { name: result.rows[0].full_name },
+//     });
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).json({ error: "Login failed" });
+//   }
 });
 
+
+router.get("/secret", (req, res) => {
+    console.log(req.isAuthenticated());
+})
 
 passport.use(new Strategy( async function verify(email, password, cb){
     try {
         const result = await db.query("SELECT * FROM users WHERE email = $1", [
             email,
           ]);
+          const user = result.rows[0];
           if (!result.rows[0])
             return res.status(404).json({ error: "User not found" });
     
@@ -88,12 +94,13 @@ passport.use(new Strategy( async function verify(email, password, cb){
             console.log("invalid credentials");
             return res.status(401).json({ error: "Invalid credentials" });
           }
-    
+         
           console.log("login success");
           res.json({
             message: "Login successful",
             user: { name: result.rows[0].full_name },
           });
+          return cb(null, user);
     } catch (error) {
         console.error(error);
         res.status(500).json({ error: "Login failed" });
