f16135691044517fbb4814443dfb8cfb6b51ea90
Allwell <allwellazubike@gmail.com>
2026-03-24 22:47:08 +0100

    paystacl bug

--- src/components/payment/Paystack.jsx
+++ src/components/payment/Paystack.jsx
@@ -1,18 +1,2 @@
 import PaystackPop from '@paystack/inline-js';
 
-export const usePaystack = () => {
-  const startPayment = ({ email, amount, metadata, onSuccess, onCancel }) => {
-    const paystack = new PaystackPop();
-    
-    paystack.newTransaction({
-      key: "pk_test_281c8c9fe122d86f7dd21779a2f1235355d6552b", // Use env variable in production
-      email,
-      amount: amount * 100, // Convert Naira to Kobo
-      metadata, // Store Cart IDs here for backend verification
-      onSuccess: (transaction) => onSuccess(transaction),
-      onCancel: () => onCancel?.(),
-    });
-  };
-
-  return { startPayment };
-};
