c04617a609b0361e2250b67cd34b46f9297c547b
Allwell <allwellazubike@gmail.com>
2026-04-03 04:06:43 +0100

    deployment

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ import orderRoutes from "./routes/orderRoutes.js";
 const app = express();
 app.use(
   cors({
-    origin: "http://localhost:5173", // Adjust if frontend port differs
+    origin: process.env.FRONTEND_URL || "http://localhost:5173", 
     credentials: true,
   }),
 );
